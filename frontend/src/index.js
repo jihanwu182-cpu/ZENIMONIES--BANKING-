68642a3aff97dcd5b8ceb2514d7699c70ede9d44
jihanwu <jihanwu182@gmail.com>
2026-09-07 22:02:26 +0200

    Fix React entry point for Render deployment

---
+++ frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import './index.css';
+import store from './store';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
