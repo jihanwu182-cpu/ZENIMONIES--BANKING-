518ac3e9a7dc5059c0e7a620fcf51d9dc30247a6
jihanwu <jihanwu182@gmail.com>
2026-09-07 16:21:45 +0200

    Add function to generate account numbers

--- backend/utils/accountNumber.js
+++ backend/utils/accountNumber.js
@@ -1,1 +1,10 @@
+const crypto = require('crypto');
 
+function generateAccountNumber(prefix = '10') {
+  const randomPart = crypto.randomInt(100000000, 1000000000);
+  return `${prefix}${randomPart}`;
+}
+
+module.exports = {
+  generateAccountNumber,
+};
