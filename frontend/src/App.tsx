aee61c098ace4248a3fd468bc16c7e53ccbbf4e3
jihanwu <jihanwu182@gmail.com>
2026-09-07 19:56:59 +0200

    Create Zenimonies main app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Zenimonies</h1>
+      <p>Your digital banking app.</p>
+    </div>
+  );
+}
+
+export default App;
