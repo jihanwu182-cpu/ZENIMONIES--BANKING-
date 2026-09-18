20389d31440fa130dc2fa0ef69ed27914381ea5a
jihanwu <jihanwu182@gmail.com>
2026-09-18 12:12:40 +0200

    Remove unnecessary await from database query

--- backend/server.js
+++ backend/server.js
@@ -564,7 +564,6 @@ const startServer = async () => {
       ADD COLUMN IF NOT EXISTS
       provider_response JSONB;
      
-      await pool.query(`
       ALTER TABLE transactions
       ADD COLUMN IF NOT EXISTS transaction_fee NUMERIC(18,2) NOT NULL DEFAULT 0.00;
     `);
