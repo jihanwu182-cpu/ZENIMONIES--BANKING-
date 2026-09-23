82a4241baa5896218471a81e1287bea72b985ffb
jihanwu <jihanwu182@gmail.com>
2026-09-23 10:07:53 +0200

    Add provider response fields to accountController

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -474,7 +474,8 @@ const getTransactions = async (
 
        bill.tariff_class
         AS bill_tariff_class
-
+      bill.provider_response AS bill_provider_response,
+      bill.provider_response_message AS bill_provider_response_message,
 
         FROM transactions t
 
